a43c2d2e7bd6a799e62d23425de1149829b48b6c
Roy Hyunjin Han <rhh@crosscompute.com>
2024-09-11 14:29:51 -0400

    Generalize get_matching_paths

--- sources/crosscompute_validation/functions/disk.py
+++ sources/crosscompute_validation/functions/disk.py
@@ -2,22 +2,13 @@ import re
 from pathlib import Path
 
 from crosscompute_macros.disk import (
-    is_existing_path,
     list_paths)
 
 
 async def get_matching_paths(path_template):
     path = Path(path_template)
-    expression = path.name
-    has_suffix = '{suffix}' in expression
-    has_index = '{index}' in expression
-    if not has_suffix and not has_index:
-        is_existing = await is_existing_path(path)
-        return [path_template] if is_existing else []
-    if has_suffix:
-        expression = expression.replace('{suffix}', '.*')
-    if has_index:
-        expression = expression.replace('{index}', '[0-9]+')
-    paths = await list_paths(path.parent)
+    expression = path.name.format(suffix='.*', index='[0-9]+')
+    folder = path.parent
+    paths = await list_paths(folder)
     pattern = re.compile(expression + '$')
-    return [_ for _ in paths if pattern.match(_)]
+    return [folder / _ for _ in paths if pattern.match(_)]
