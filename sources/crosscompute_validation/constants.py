bc59bfc19fc27b8be9e0f58e64bdcc9feb0dbdb6
Roy Hyunjin Han <rhh@crosscompute.com>
2025-06-23 23:05:44 -0400

    Increase RAW_DATA_BYTE_COUNT

--- sources/crosscompute_validation/constants.py
+++ sources/crosscompute_validation/constants.py
@@ -22,7 +22,7 @@ VARIABLE_ID_PATTERN = re.compile(r'[a-zA-Z0-9_]+$')
 VARIABLE_ID_TEMPLATE_PATTERN = re.compile(r'{ *([a-zA-Z0-9_| ]+?) *}')
 
 
-RAW_DATA_BYTE_COUNT = 1024
+RAW_DATA_BYTE_COUNT = 256 * 1024
 RAW_DATA_CACHE_LENGTH = 256
 
 
